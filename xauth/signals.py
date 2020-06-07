a05f6f8ef55fceb64aff781c11cdc640b0a92373
Orinda Harrison <ajuongharry35@gmail.com>
2020-06-07 16:30:11 +0300

    Remove dead signals code

--- xauth/signals.py
+++ xauth/signals.py
@@ -1,22 +1,7 @@
 from django.contrib.auth import get_user_model
-from django.db import IntegrityError
-from django.db.models.signals import pre_save, post_save, post_migrate
-from django.utils import timezone
+from django.db.models.signals import post_save, post_migrate
 
-from xauth.models import AccessLog, SecurityQuestion
-
-
-def on_user_pre_save(sender, instance, **kwargs):
-    """creates access & password reset log(s)"""
-    try:
-        user = get_user_model().objects.get(pk=instance.pk)
-        last_login = user.last_login
-        if last_login and last_login != instance.last_login:
-            # the user probably just logged in
-            AccessLog.objects.create(user=user, sign_in_time=timezone.now())
-
-    except get_user_model().DoesNotExist:
-        pass
+from xauth.models import SecurityQuestion
 
 
 def on_user_post_save(sender, instance, created, **kwargs):
@@ -29,12 +14,8 @@ def on_user_post_save(sender, instance, created, **kwargs):
 
 
 def on_post_migrate(sender, **kwargs):
-    try:
-        SecurityQuestion.objects.get_or_create(question='Default', usable=False, )
-    except IntegrityError:
-        pass
+    SecurityQuestion.objects.get_or_create(question='Default', usable=False, )
 
 
-pre_save.connect(on_user_pre_save, sender=get_user_model(), dispatch_uid='1')
-post_save.connect(on_user_post_save, sender=get_user_model(), dispatch_uid='2')
-post_migrate.connect(on_post_migrate, dispatch_uid='3')
+post_save.connect(on_user_post_save, sender=get_user_model(), dispatch_uid='1')
+post_migrate.connect(on_post_migrate, dispatch_uid='2')
