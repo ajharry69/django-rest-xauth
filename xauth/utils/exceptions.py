46dbd6bbf48008158fd61256d202a7f3a88cf284
Orinda Harrison <ajuongharry35@gmail.com>
2020-07-10 13:07:22 +0300

    combine extra errors with existing errors to produce a list of unique errors

--- xauth/utils/exceptions.py
+++ xauth/utils/exceptions.py
@@ -6,7 +6,9 @@ from xauth.utils.response import ErrorResponse
 
 
 def wrap_error_response_data(ed, **kwargs):
-    msg, d_msg, metadata, delimiter = ed, None, None, '#'
+    errors = kwargs.get('errors', tuple())
+    d_msg = errors[0] if len(errors) > 0 else None
+    msg, metadata, delimiter = ed, None, '#'
     if delimiter in ed:
         msg, d_msg = tuple(ed.split(delimiter, 1))
         if delimiter in d_msg:
@@ -16,10 +18,11 @@ def wrap_error_response_data(ed, **kwargs):
     if valid_str(msg) and valid_str(detail) and msg.lower() == detail.lower():
         # avoids duplicate showing of the same error message
         kwargs['detail'] = None
+    # combine extra errors with existing errors to produce a list of unique errors
+    kwargs['errors'] = list(set(errors + tuple(extra_errors))) if extra_errors and isinstance(errors, tuple) else errors
     return ErrorResponse(
         message=msg,
         debug_message=d_msg,
-        extra_errors=extra_errors,
         **kwargs,
     ).data
 
