1a7422b77c9d5e42d9d0beb6eafede569f51d647
Orinda Harrison <ajuongharry35@gmail.com>
2021-08-03 19:28:20 +0300

    Change default location of keys

--- settings.py
+++ settings.py
@@ -5,6 +5,8 @@ from xauth.settings import *  # noqa
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
 
+XAUTH_KEYS_DIR = BASE_DIR / ".secrets"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
